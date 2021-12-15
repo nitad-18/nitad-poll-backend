69561150d48aa6df76311cc7b275e4bed059aae5
samithiwat <samithiwat2544@gmail.com>
2021-12-16 03:02:56 +0700

    Add User Resources and Migration File

---
+++ src/user/dto/create-user.dto.ts
@@ -0,0 +1,14 @@
+import { IsNotEmpty, Length, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  username: string;
+
+  @IsNotEmpty()
+  @MinLength(8, { message: 'Password must at least 8 characters' })
+  password: string;
+
+  @IsNotEmpty()
+  @Length(1, 15, { message: 'Displayname must between 1 and 15 characters' })
+  displayName: string;
+}
