1f4c25afb34265861023194d7195aa549f4b25be
samithiwat <samithiwat2544@gmail.com>
2021-12-17 02:27:45 +0700

    Add Vote Function

--- src/database/migrations/1639666786911-add_relationship_poll_poll_option.ts
+++ src/database/migrations/1639666786911-add_relationship_poll_poll_option.ts
@@ -1,16 +1,19 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class addRelationshipPollPollOption1639666786911 implements MigrationInterface {
-    name = 'addRelationshipPollPollOption1639666786911'
+  name = 'addRelationshipPollPollOption1639666786911';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "poll_option" ADD "pollId" integer`);
-        await queryRunner.query(`ALTER TABLE "poll_option" ADD CONSTRAINT "FK_a1200fcfcdab6145351545f26ea" FOREIGN KEY ("pollId") REFERENCES "poll"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "poll_option" ADD "pollId" integer`);
+    await queryRunner.query(
+      `ALTER TABLE "poll_option" ADD CONSTRAINT "FK_a1200fcfcdab6145351545f26ea" FOREIGN KEY ("pollId") REFERENCES "poll"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "poll_option" DROP CONSTRAINT "FK_a1200fcfcdab6145351545f26ea"`);
-        await queryRunner.query(`ALTER TABLE "poll_option" DROP COLUMN "pollId"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "poll_option" DROP CONSTRAINT "FK_a1200fcfcdab6145351545f26ea"`,
+    );
+    await queryRunner.query(`ALTER TABLE "poll_option" DROP COLUMN "pollId"`);
+  }
 }
