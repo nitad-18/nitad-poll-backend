2f05f9d89005e4fd3d7c6e447ddeacfaf145d6d4
samithiwat <samithiwat2544@gmail.com>
2021-12-16 21:36:46 +0700

    Add Type

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Delete, Get, Post, Req, Res, UseGuards } from '@nestjs/common';
-import { User } from 'src/user/entities/user.entity';
+import { Response } from 'express';
 import { UserService } from 'src/user/user.service';
+import { UserData } from 'src/utilities/type';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { RegisterDto } from './dto/register.dto';
@@ -11,18 +12,16 @@ export class AuthController {
   constructor(private readonly authService: AuthService, private userService: UserService) {}
 
   @Post('register')
-  async register(@Body() registerDto: RegisterDto, @Res() res) {
-    const user: User = await this.userService.create(registerDto);
+  async register(@Body() registerDto: RegisterDto, @Res() res: Response) {
+    const user: UserData = await this.userService.create(registerDto);
     if (!user) {
       return res.status(422).json({ message: 'This username have been already existed' });
     }
-
-    const { password, createdData, deletedDate, updatedDate, username, ...result } = user;
-    return res.status(201).json({ message: 'Successfully registered user', ...result });
+    return res.status(201).json({ message: 'Successfully registered user', ...user });
   }
 
   @Post('login')
-  async login(@Res() res, @Body() loginDto: LoginDto) {
+  async login(@Body() loginDto: LoginDto, @Res() res: Response) {
     const user = await this.userService.findOne(loginDto);
     if (!user) {
       return res.status(401).json({ message: 'Unauthorize' });
@@ -34,15 +33,14 @@ export class AuthController {
 
   @Get('me')
   @UseGuards(JwtAuthGuard)
-  async profile(@Req() req, @Res() res) {
-    const { password, createdData, deletedDate, updatedDate, ...result } =
-      await this.userService.findById(req.user.id);
-    return res.status(200).json(result);
+  async profile(@Req() req, @Res() res: Response) {
+    const user = await this.userService.findById(req.user.id);
+    return res.status(200).json(user);
   }
 
   @Delete('logout')
   @UseGuards(JwtAuthGuard)
-  logout(@Res() res) {
+  logout(@Res() res: Response) {
     res.clearCookie('access_token');
     return res.status(204).send();
   }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -4,14 +4,15 @@ import * as bcrypt from 'bcrypt';
 import { EditProfileDto } from 'src/auth/dto/edit-profile.dto';
 import { LoginDto } from 'src/auth/dto/login.dto';
 import { RegisterDto } from 'src/auth/dto/register.dto';
-import { Repository } from 'typeorm';
+import { UserData } from 'src/utilities/type';
+import { DeleteResult, Repository, UpdateResult } from 'typeorm';
 import { User } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
   constructor(@InjectRepository(User) private userRepository: Repository<User>) {}
 
-  async create(registerDto: RegisterDto): Promise<User | null> {
+  async create(registerDto: RegisterDto): Promise<UserData | null> {
     const userCount = await this.userRepository
       .createQueryBuilder()
       .select('user')
@@ -27,28 +28,44 @@ export class UserService {
     registerDto.password = await bcrypt.hash(registerDto.password, saltRounds);
 
     const user = this.userRepository.create(registerDto);
+    const { createdDate, deletedDate, updatedDate, password, ...result } =
+      await this.userRepository.save(user);
 
-    return await this.userRepository.save(user);
+    return result;
   }
 
   async findAll(): Promise<User[]> {
-    return await this.userRepository.find();
+    return await this.userRepository.find({ select: ['id', 'username', 'displayName'] });
   }
 
-  async findOne(loginDto: LoginDto): Promise<User | null> {
+  async findOne(loginDto: LoginDto): Promise<UserData | null> {
     let user: User;
     try {
-      user = await this.userRepository.findOneOrFail({ username: loginDto.username });
+      user = await this.userRepository.findOneOrFail(
+        { username: loginDto.username },
+        { select: ['id', 'username', 'displayName', 'password'] },
+      );
     } catch (err) {
       return null;
     }
     if (!(await bcrypt.compare(loginDto.password, user.password))) {
       return null;
     }
-    return user;
+    const { password, ...result } = user;
+    return result;
   }
 
-  async findById(id: number): Promise<User> {
+  async findById(id: number): Promise<User | null> {
+    try {
+      return await this.userRepository.findOneOrFail(id, {
+        select: ['id', 'displayName', 'username'],
+      });
+    } catch (err) {
+      return null;
+    }
+  }
+
+  async getAllUserDataByID(id: number): Promise<User> {
     try {
       return await this.userRepository.findOneOrFail(id);
     } catch (err) {
@@ -56,7 +73,7 @@ export class UserService {
     }
   }
 
-  async update(id: number, editProfileDto: EditProfileDto) {
+  async update(id: number, editProfileDto: EditProfileDto): Promise<UpdateResult | null> {
     try {
       return await this.userRepository.update(id, editProfileDto);
     } catch (err) {
@@ -64,7 +81,7 @@ export class UserService {
     }
   }
 
-  async remove(id: number) {
+  async remove(id: number): Promise<DeleteResult | null> {
     try {
       return await this.userRepository.softDelete(id);
     } catch (err) {
