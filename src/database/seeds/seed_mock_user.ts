8408792b29257001f71f1d13a826d269d1d22e4c
samithiwat <samithiwat2544@gmail.com>
2021-12-17 18:19:25 +0700

    Add Factory and Seeder of Mock Data

---
+++ src/database/seeds/seed_mock_user.ts
@@ -0,0 +1,12 @@
+import * as bcrypt from 'bcrypt';
+import { User } from 'src/user/entities/user.entity';
+import { Connection } from 'typeorm';
+import { Factory, Seeder } from 'typeorm-seeding';
+
+export default class SeedMockUser implements Seeder {
+  public async run(factory: Factory, connection: Connection): Promise<void> {
+    const SALTROUND = 10;
+    const password = await bcrypt.hash('adminadmin', SALTROUND);
+    await factory(User)().createMany(10, { password: password });
+  }
+}
