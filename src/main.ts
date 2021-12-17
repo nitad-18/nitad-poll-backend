280f9e7787b2edf61b202f17fbba646f9248b6c8
samithiwat <samithiwat2544@gmail.com>
2021-12-18 02:16:36 +0700

    Fix CORS Error

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
 
