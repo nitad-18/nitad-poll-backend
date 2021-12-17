851200d66446adc0efeefceee9f8def262af4d36
samithiwat <samithiwat2544@gmail.com>
2021-12-18 03:31:45 +0700

    Add user Test Unit

---
+++ test/config/typeorm.ts
@@ -0,0 +1,28 @@
+import * as dotenv from 'dotenv';
+import { ConnectionOptions } from 'typeorm';
+
+const envType = process.env.MODE_ENV || 'development';
+
+dotenv.config({ path: `.env.${envType}` });
+
+type ConnectionOptionsWithSeed = ConnectionOptions & {
+  seeds: string[];
+  factories: string[];
+};
+
+const options: ConnectionOptionsWithSeed = {
+  type: 'sqlite',
+  database: '../test.sqlite',
+  dropSchema: true,
+  synchronize: true,
+  logging: false,
+  entities: ['src/**/entities/*.entity{.ts,.js}'],
+  migrations: ['src/database/migrations/*{.ts,.js}'],
+  seeds: ['src/database/seeds/*{.ts,.js}'],
+  factories: ['src/database/factories/*{.ts,.js}'],
+  cli: {
+    migrationsDir: 'src/database/migrations',
+  },
+};
+
+export = options;
