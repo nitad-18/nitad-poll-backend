ba138a71da9f158cee6add4e202ec8b2f79fefdf
samithiwat <samithiwat2544@gmail.com>
2021-12-18 12:23:51 +0700

    Edit name of seeder

--- src/database/seeds/1639804862-seed_mock_poll.ts
+++ src/database/seeds/1639804862-seed_mock_poll.ts
@@ -8,7 +8,7 @@ import { Factory, Seeder } from 'typeorm-seeding';
 export default class SeedMockPoll implements Seeder {
   public async run(factory: Factory, connection: Connection): Promise<void> {
     const users: User[] = await connection.getRepository(User).find();
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 10; i++) {
       const userIdx = faker.datatype.number({ max: users.length - 1 });
       const poll = await factory(Poll)({ author: users[userIdx] }).create();
       await factory(PollOption)({
