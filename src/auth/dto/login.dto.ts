bd4abcd01f916358083c9778f3e4bbf14e355a39
samithiwat <samithiwat2544@gmail.com>
2021-12-19 01:29:35 +0700

    Add userDto's validation

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,9 +1,13 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, MinLength } from 'class-validator';
 
 export class LoginDto {
   @ApiProperty()
+  @IsNotEmpty()
   username: string;
 
   @ApiProperty()
+  @IsNotEmpty()
+  @MinLength(8)
   password: string;
 }
