2fa5b020ff1579780694168b98b7268712637c85
samithiwat <samithiwat2544@gmail.com>
2021-12-18 12:23:51 +0700

    Edit name of seeder

--- src/database/seeds/1639804824-seed_mock_user.ts
+++ src/database/seeds/1639804824-seed_mock_user.ts
@@ -7,6 +7,6 @@ export default class SeedMockUser implements Seeder {
   public async run(factory: Factory, connection: Connection): Promise<void> {
     const SALTROUND = 10;
     const password = await bcrypt.hash('adminadmin', SALTROUND);
-    await factory(User)().createMany(10, { password: password });
+    await factory(User)().createMany(20, { password: password });
   }
 }
