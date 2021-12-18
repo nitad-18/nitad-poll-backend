aa34b822f61a8b1051725e2afb2424790314d0a7
samithiwat <samithiwat2544@gmail.com>
2021-12-18 11:54:24 +0700

    Fix Spelling in Config

--- src/config/config.ts
+++ src/config/config.ts
@@ -10,8 +10,8 @@ export default () => ({
   secret: process.env.SECRET,
   database: {
     type: process.env.DATABASE_TYPE,
-    host: process.env.DATABSE_HOST || 'localhost',
-    port: parseInt(process.env.DATABSE_PORT) || 5432,
+    host: process.env.DATABASE_HOST || 'localhost',
+    port: parseInt(process.env.DATABASE_PORT) || 5432,
     name: process.env.DATABASE_NAME,
     username: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
