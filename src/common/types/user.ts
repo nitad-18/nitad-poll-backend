7b34d1ab352bde11c6ddbdbc59dce320954b8cb5
samithiwat <samithiwat2544@gmail.com>
2021-12-20 18:02:14 +0700

    Remove votedPolls and polls from userData type

--- src/common/types/user.ts
+++ src/common/types/user.ts
@@ -1,9 +1,5 @@
-import { Poll } from 'src/poll/entities/poll.entity';
-
 export type UserData = {
   id: number;
   username: string;
   displayName: string;
-  votedPolls?: Poll[];
-  polls?: Poll[];
 };
