d7e7490bad21f860342dfb7daac35e68833657f5
samithiwat <samithiwat2544@gmail.com>
2021-12-23 01:07:30 +0700

    Fix Error

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,13 +1,5 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, MinLength } from 'class-validator';
-
 export class LoginDto {
-  @ApiProperty()
-  @IsNotEmpty()
   username: string;
 
-  @ApiProperty()
-  @IsNotEmpty()
-  @MinLength(8)
   password: string;
 }
