33ee6d9edc8432823182c2a901727539e7c968c0
samithiwat <samithiwat2544@gmail.com>
2021-12-23 01:18:05 +0700

    Fix Error

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,4 @@
-// import {
-//   Body,
-//   Controller,
-//   Delete,
-//   Get,
-//   HttpCode,
-//   HttpStatus,
-//   Post,
-//   Req,
-//   Res,
-//   UseGuards,
-// } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 // import { Response } from 'express';
 // import { UserData } from 'src/common/types/user';
 // import { UserService } from 'src/user/user.service';
@@ -18,38 +7,34 @@
 // import { RegisterDto } from './dto/register.dto';
 // import { JwtAuthGuard } from './jwt-auth.guard';
 
-// @Controller('auth')
-// export class AuthController {
-//   constructor(private readonly authService: AuthService, private userService: UserService) {}
-
-//   @Post('register')
-//   async register(@Body() registerDto: RegisterDto, @Res() res: Response): Promise<Response> {
-//     const user: UserData = await this.userService.create(registerDto);
-//     return res
-//       .status(HttpStatus.CREATED)
-//       .json({ message: 'Successfully registered user', ...user });
-//   }
-
-//   @Post('login')
-//   async login(@Body() loginDto: LoginDto, @Res() res: Response): Promise<Response> {
-//     const user = await this.userService.findOne(loginDto);
-//     const token: string = await this.authService.createToken(user);
-//     res.cookie('access_token', token, { httpOnly: true, secure: false });
-//     return res.status(HttpStatus.OK).json(user);
-//   }
-
-//   @Get('me')
-//   @UseGuards(JwtAuthGuard)
-//   async profile(@Req() req, @Res() res: Response): Promise<Response> {
-//     const user = await this.userService.findById(req.user.id);
-//     return res.status(HttpStatus.OK).json(user);
-//   }
-
-//   @Delete('logout')
-//   @HttpCode(HttpStatus.NO_CONTENT)
-//   @UseGuards(JwtAuthGuard)
-//   logout(@Res() res: Response): Response {
-//     res.clearCookie('access_token');
-//     return res.send();
-//   }
-// }
+@Controller('auth')
+export class AuthController {
+  //   constructor(private readonly authService: AuthService, private userService: UserService) {}
+  //   @Post('register')
+  //   async register(@Body() registerDto: RegisterDto, @Res() res: Response): Promise<Response> {
+  //     const user: UserData = await this.userService.create(registerDto);
+  //     return res
+  //       .status(HttpStatus.CREATED)
+  //       .json({ message: 'Successfully registered user', ...user });
+  //   }
+  //   @Post('login')
+  //   async login(@Body() loginDto: LoginDto, @Res() res: Response): Promise<Response> {
+  //     const user = await this.userService.findOne(loginDto);
+  //     const token: string = await this.authService.createToken(user);
+  //     res.cookie('access_token', token, { httpOnly: true, secure: false });
+  //     return res.status(HttpStatus.OK).json(user);
+  //   }
+  //   @Get('me')
+  //   @UseGuards(JwtAuthGuard)
+  //   async profile(@Req() req, @Res() res: Response): Promise<Response> {
+  //     const user = await this.userService.findById(req.user.id);
+  //     return res.status(HttpStatus.OK).json(user);
+  //   }
+  //   @Delete('logout')
+  //   @HttpCode(HttpStatus.NO_CONTENT)
+  //   @UseGuards(JwtAuthGuard)
+  //   logout(@Res() res: Response): Response {
+  //     res.clearCookie('access_token');
+  //     return res.send();
+  //   }
+}
